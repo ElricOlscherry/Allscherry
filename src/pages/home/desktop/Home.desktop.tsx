08dbf0c53455287459b2aac289d96fa620fdc73d
Hayden Zeller <me@haydenzeller.dev>
2024-01-06 15:23:00 -0500

    Netliyay

--- src/pages/home/desktop/Home.desktop.tsx
+++ src/pages/home/desktop/Home.desktop.tsx
@@ -215,22 +215,20 @@ export default function HomeDesktop () {
                 </div>
             </div>
             <form name="contact" method="post">
-      <p>
-        <label htmlFor="name">Name</label> <br />
-        <input type="text" id="name" name="name" required />
-      </p>
-      <p>
-        <label htmlFor="email">Email</label> <br />
-        <input type="email" id="email" name="email" required />
-      </p>
-      <p>
-        <label htmlFor="message">Message</label> <br />
-        <textarea id="message" name="message" required></textarea>
-      </p>
-      <p>
-        <input type="submit" value="Submit message" />
-      </p>
-    </form>
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>Your Name: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
         </div>
     )
 }
